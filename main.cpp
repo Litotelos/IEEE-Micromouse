4e30d84f28e7e4b4e84aae25daea96ef58a23fe6
beyza ali <160631363+beyzaly@users.noreply.github.com>
2025-02-28 12:06:52 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
  std::cout << "Hello world" << std::endl;
  cout << "Making a rando commit because ty forced me to idk if this counts as a commit or not" << endl;
  cout << "Ngl this is fun i got one commit this is gonna be my second one he he " << endl;
+  cout << "Do not panick we can delete these" << endl;
     return 0;
 }
 
