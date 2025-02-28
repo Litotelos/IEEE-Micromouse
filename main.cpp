75d47af9b111b0a53579b9d6fc2803eac7245837
beyza ali <160631363+beyzaly@users.noreply.github.com>
2025-02-28 12:05:17 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-
+using namepsace std;
 int main() {
  std::cout << "Hello world" << std::endl;
+ cout << "Making a rando commit because ty forced me to idk if this counts as a commit or not";
     return 0;
 }
 
