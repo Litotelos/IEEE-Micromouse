f1921ccf348af4f622be7ae88cf8e817d43b4fdd
beyza ali <160631363+beyzaly@users.noreply.github.com>
2025-03-06 15:42:37 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namepsace std;
 int main() {
  std::cout << "Hello world" << std::endl;
  cout << "Making a rando commit because ty forced me to idk if this counts as a commit or not" << endl;
+ cout << "i need to start looking at code for this project ahh"
 
  
     return 0;
