64111d4275c1aeae8f89d47ca9e3af86e2de0680
Litotelos <Tyrese_smith@sent.com>
2025-03-19 06:37:15 +0000

    Removed buffer flushing and omitted return type.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-using namepsace std;
+
 int main() {
- std::cout << "Hello world" << std::endl;
- cout << "Making a rando commit because ty forced me to idk if this counts as a commit or not" << endl;
- cout << "rando 2" << endl;
-
-
-    return 0;
-}//ff
+ std::cout << "Making a rando commit because ty forced me to idk if this counts as a commit or not" << '\n';
+ std::cout << "rando 2" << '\n';
+}
 
